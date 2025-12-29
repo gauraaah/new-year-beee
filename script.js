8ec26b1a664750ea68a0a9db43a8157bf56a5e33
gauraaah <gauravpmahajan2005@gmail.com>
2025-12-29 17:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.body.addEventListener("click", () => {
-  alert("Site is working");
+const music = document.getElementById("bgMusic");
+const screen = document.body;
+
+let started = false;
+
+screen.addEventListener("click", () => {
+  if (!started) {
+    music.volume = 0.8;
+    music.play().catch(() => {});
+    started = true;
+  }
 });
