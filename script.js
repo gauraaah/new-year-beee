cb2d5276f6eaf78d41ff1c93f087a5b6585d412e
gauraaah <gauravpmahajan2005@gmail.com>
2025-12-29 17:12:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+let currentScene = 1;
+
+const scene1 = document.getElementById("scene1");
+const scene2 = document.getElementById("scene2");
 const music = document.getElementById("bgMusic");
 
-let started = false;
-
 document.body.addEventListener("click", () => {
-  if (!started) {
-    music.volume = 0.8;
+  if (currentScene === 1) {
+    // start music (required user interaction)
     music.play().catch(() => {});
-    started = true;
+
+    scene1.classList.remove("active");
+    scene2.classList.add("active");
+
+    currentScene = 2;
   }
 });
